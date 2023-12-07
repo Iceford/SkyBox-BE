c60c93c54273b26bc4d647bd7c563c81510e8e07
WhimsyQuester <rongquanhuang01@gmail.com>
2023-12-07 16:08:31 +0800

    Preview page

---
+++ SkyBox-FE/src/components/preview/PreviewPdf.vue
@@ -0,0 +1,70 @@
+<!--
+ * @Description: 用于在网页中嵌入和预览PDF文件
+ * @FilePath: \SkyBox-FE\src\components\preview\PreviewPdf.vue
+ * @Author: hhrwvyy5654v huang_rongquan@outlook.com
+ * @Date: 2023-11-14 15:32:40
+ * @LastEditors: WhimsyQuester rongquanhuang01@gmail.com
+ * @LastEditTime: 2023-12-07 15:29:55
+ * Copyright (c) 2023 by hhrwvyy5654v , All Rights Reserved. 
+-->
+<!-- HTML模板部分 -->
+<template>
+    <div class="pdf">
+        <vue-pdf-embed ref="pdfRef" :source="state.url" class="vue-pdf-embed" width="850" :page="state.pageNum"
+            :style="scaleFun" />
+    </div>
+</template>
+
+<!-- 脚本部分 -->
+<script setup>
+import VuePdfEmbed from "vue-pdf-embed";
+import { createLoadingTask } from "vue3-pdfjs";
+import { ElLoading } from "element-plus";
+
+import { computed, getCurrentInstance, reactive } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const { proxy } = getCurrentInstance();
+const router = useRouter();
+const route = useRoute();
+
+const props = defineProps({
+    url: {
+        type: String,
+    },
+});
+const scaleFun = computed(() => {
+    return "transform:scale(${state.scale})";
+});
+
+const state = reactive({
+    url: "", // 预览pdf文件地址
+    pageNum: 0, // 当前页面
+    numPages: 0, // 总页数
+});
+
+const init = () => {
+    const url = "/api" + props.url;
+    state.url = url;
+    const loading = ElLoading.service({
+        lock: true,
+        text: "加载中......",
+        background: "rgba(0, 0, 0, 0.7)",
+    });
+    const loadingTask = createLoadingTask(state.url);
+    loadingTask.promise.then((pdf) => {
+        loading.close();
+        state.numPages = pdf.numPages;
+    });
+};
+
+init();
+</script>
+
+<!-- 样式部分 -->
+<style lang="scss" scoped>
+.pdf {
+    width: 100%;
+}
+</style>
+
